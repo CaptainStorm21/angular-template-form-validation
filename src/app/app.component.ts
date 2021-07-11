77562567cabd1e31b63b74833afcd563be244d8f
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-07-11 00:29:12 -0700

    validations and disable  Submit disabled if invalid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  email: string;
-  
+  email: string = '';
+
+  onSubmit() {
+    console.log(this.email);
+  }
+
+
 
 }
